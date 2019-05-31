e1f7c30ce8b7f69e3b55feedbb7a4185239e0b37
Herna Michel <labellehernamichel@yahoo.fr>
2019-05-31 13:09:19 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,9 +3,12 @@ class Application < Sinatra::Base
   get '/' do
     erb :index
   end
+<<<<<<< HEAD
   
    # New route to respond to the form submission
   post '/greet' do
     erb :greet
   end
+=======
+>>>>>>> 48f02be2ad13c126a6b42cfb8c16fd4ef173b188
 end
